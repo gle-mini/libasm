23545b3e48397c6dbc07313486d9c57adac61247
mathieu <mathieu@2a02-8440-3204-9548-66bc-58ff-fe76-289d.rev.sfr.net>
2025-04-10 02:02:47 +0200

    fix: ft_list_push_front pass all the tests OK

--- ft_list_push_front.s
+++ ft_list_push_front.s
@@ -1,35 +1,78 @@
-; ft_list_push_front.s
-; Function: void ft_list_push_front(t_list **begin_list, void *data)
-; Input:
-;    RDI = pointer to the head pointer (t_list **)
-;    RSI = data pointer to store in the new node
-; Uses:
-;    malloc – to allocate 16 bytes for a new node
-; t_list structure:
-;    offset 0: data (8 bytes)
-;    offset 8: next (8 bytes)
+; Define the t_list structure constants if not already defined.
+%define t_list_size 16    ; 16 bytes for a node (2 pointers, 8 bytes each)
+%define t_list.data 0     ; offset 0 for data
+%define t_list.next 8     ; offset 8 for next pointer
 
 section .text
+    global ft_create_elem
     global ft_list_push_front
     extern malloc
+    extern __errno_location
 
+;----------------------------------------------------------------------------
+; ft_create_elem
+; Creates a new list node.
+;
+; Input:
+;   RDI = pointer to the data to be stored in the new node.
+; Output:
+;   RAX = pointer to the new node or 0 on failure.
+;
+; On failure, it calls __errno_location and sets errno to ENOMEM (12).
+;----------------------------------------------------------------------------
+ft_create_elem:
+    ; Save the original data pointer
+    push    rdi
+
+    ; Allocate memory for the new node (16 bytes)
+    mov     rdi, t_list_size
+    call    malloc wrt ..plt
+    pop     rdi              ; restore the data pointer
+
+    test    rax, rax
+    jz      .enomem         ; if malloc returned 0, jump to error
+
+    ; Initialize the node:
+    ; Store the data pointer at offset 0 of the node.
+    mov     qword [rax + t_list.data], rdi
+    ; Set the next pointer to 0 (NULL).
+    mov     qword [rax + t_list.next], 0
+    ret
+
+.enomem:
+    ; Call __errno_location to get the pointer to errno.
+    call    __errno_location wrt ..plt
+    ; Set errno to ENOMEM (12)
+    mov     byte [rax], 12
+    xor     rax, rax       ; return 0 to indicate failure.
+    ret
+
+;----------------------------------------------------------------------------
+; ft_list_push_front
+; Adds a new node at the beginning of the linked list.
+;
+; Input:
+;   RDI = pointer to the head pointer (t_list **)
+;   RSI = data pointer to store in the new node.
+;----------------------------------------------------------------------------
 ft_list_push_front:
-    push rbx                ; save RBX (callee-saved)
-    push r12                ; save R12 (we'll use it to hold begin_list)
-    mov r12, rdi            ; r12 = pointer to begin_list
-    mov rdi, 16             ; allocate 16 bytes (size of t_list)
-    call malloc wrt ..plt             ; new node pointer in RAX (or 0 on failure)
-    test rax, rax
-    je .done                ; if allocation failed, do nothing
-    mov rbx, rax            ; rbx = new node pointer
-    ; new->data = data (second parameter, RSI)
-    mov [rbx], rsi
-    ; new->next = *begin_list
-    mov rcx, [r12]         ; use r12 which holds the begin_list pointer
-    mov [rbx+8], rcx
-    ; update head pointer: *begin_list = new node
-    mov [r12], rbx
+    ; If the pointer to the list (begin_list) is NULL, do nothing.
+    test    rdi, rdi
+    jz      .done
+
+    ; Create a new node with the data.
+    push    rdi            ; save the pointer to the head pointer
+    mov     rdi, rsi       ; set the data pointer as the argument for ft_create_elem
+    call    ft_create_elem
+    pop     rdi            ; restore the pointer to the head pointer
+    test    rax, rax
+    jz      .done          ; if node creation failed, return
+
+    ; rax now contains the pointer to the new node.
+    ; Link the new node into the list:
+    mov     rsi, [rdi]     ; load current head pointer
+    mov     qword [rax + t_list.next], rsi  ; new->next = current head
+    mov     qword [rdi], rax ; update head pointer to point to the new node
+
 .done:
-    pop r12
-    pop rbx
     ret
